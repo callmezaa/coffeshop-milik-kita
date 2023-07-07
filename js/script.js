59f0b3baea42ce3bab981a34476562e40e324898
kenzny <kenzamariyan32@gmail.com>
2023-07-07 21:44:01 +0700

    membuat coffeshop miki kita

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Toggle clas active
+const navbarNav = document.querySelector(".navbar-nav");
+
+//ketika hamburger menu diklik
+
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//hilangkan nav
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
